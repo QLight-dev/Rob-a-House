239a0cf9c47277b6b90e70dd19b514bb68c66511
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-08-19 19:55:58 +0400

    Added proper controls for player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private InputSystem_Actions controls;
+    public int speed = 10;
+
+    void Awake()
+    {
+        controls = new InputSystem_Actions();
+    }
+
+    private void OnEnable()
+    {
+        controls.Player.Enable();
+    }
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 1.5f, 0);
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = controls.Player.Move.ReadValue<Vector2>();
+        transform.Translate(
+            moveInput.x * speed * Time.deltaTime,
+            0,
+            moveInput.y * speed * Time.deltaTime
+        );
+
+        if (transform.position.z > 4.5)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 4.5f);
+        }
+        if (transform.position.z < -4.5f)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -4.5f);
+        }
+
+        if (transform.position.x > 20.85f)
+        {
+            transform.position = new Vector3(20.85f, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < -20.85f)
+        {
+            transform.position = new Vector3(-20.85f, transform.position.y, transform.position.z);
+        }
+    }
+}
