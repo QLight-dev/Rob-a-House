2f580e6e5cf305c9561a3baa0a205a7ccc366639
Muhammed Uzair <nayazmuhammed094@gmail.com>
2026-08-20 19:58:20 +0400

    Imported TMP and created coroutines for the folding and unfolding

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Spike : MonoBehaviour
@@ -7,26 +8,59 @@ public class Spike : MonoBehaviour
     [SerializeField]
     private int spikeSpeed;
 
-    void Start() { }
+    void Start()
+    {
+    }
 
-    void Update() { }
+    void Update()
+    {
+
+    }
 
     void Unfold()
     {
-        transform.position = Vector3.Lerp(
-            transform.position,
-            new Vector3(transform.position.x, transform.position.y, -1),
-            spikeSpeed * Time.deltaTime
-        );
+        StartCoroutine(UnfoldCoroutine());
     }
 
     void Fold()
     {
-        transform.position = Vector3.Lerp(
-            transform.position,
-            new Vector3(transform.position.x, transform.position.y, -6),
-            spikeSpeed * Time.deltaTime
-        );
+        StartCoroutine(FoldCoroutine());
     }
 
+    private IEnumerator UnfoldCoroutine()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < 0.15f)
+        {
+            elapsed += Time.deltaTime;
+            transform.position = Vector3.Lerp(
+                transform.position,
+                new Vector3(transform.position.x, transform.position.y, -1),
+                spikeSpeed * Time.deltaTime
+            );
+            yield return null;
+        }
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, -1);
+    }
+
+
+    private IEnumerator FoldCoroutine()
+    {
+        float elapsed = 0f;
+
+        while (elapsed < 0.15f)
+        {
+            elapsed += Time.deltaTime;
+            transform.position = Vector3.Lerp(
+                transform.position,
+                new Vector3(transform.position.x, transform.position.y, -6),
+                spikeSpeed * Time.deltaTime
+            );
+            yield return null;
+        }
+
+        transform.position = new Vector3(transform.position.x, transform.position.y, -6);
+    }
 }
